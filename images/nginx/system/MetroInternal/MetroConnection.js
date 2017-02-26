86714012c7f471c5a4d0619ad422d95e6d5f9c52
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-02-26 18:26:50 -0500

    Split up class and index for MetroConnection

---
+++ images/nginx/system/MetroInternal/MetroConnection.js
@@ -0,0 +1,49 @@
+var MetroStream = require('./MetroStream.js')
+
+class MetroConnection {
+  constructor () {
+    this.buffer = ''
+    this.events = {
+      data: [],
+      die: []
+    }
+
+    process.stdin.on('data', (chunk) => {
+      // If null terminator, reset buffer
+      if (chunk[0] == 0) { this.buffer = ''; return }
+
+      // Add data to input buffer
+      this.buffer += chunk
+
+      // If current buffer value is valid JSON, clear buffer and run events
+      let body;
+      try {
+        body = JSON.parse(this.buffer)
+      } catch (e) { return }
+      this.buffer = ''
+      this.events.data.forEach((post) => post(body))
+    })
+  }
+
+  /* MetroConnection API */
+
+  on (event) {
+    return new MetroStream((post, kill) => {
+      if (Object.keys(this.events).indexOf(event) !== -1) {
+        this.events[event].push(post)
+        this.events.die.push(kill)
+      }
+    })
+  }
+
+  command (command) {
+    return this.on('data')
+               .filter((data) => data.is === command)
+               .replace((data) => {
+                 delete data.is
+                 return data
+               })
+  }
+}
+
+module.exports = MetroConnection
--- images/nginx/system/index.js
+++ images/nginx/system/index.js
@@ -1,63 +1,8 @@
-#!/usr/bin/env node
-
-var MetroStream = require('./MetroInternal/MetroStream.js')
-
-class MetroConnection {
-  constructor () {
-    this.buffer = ''
-    this.events = {
-      data: [],
-      die: []
-    }
-
-    process.stdin.on('data', (chunk) => {
-      // If null terminator, reset buffer
-      if (chunk[0] == 0) { this.buffer = ''; return }
-
-      // Add data to input buffer
-      this.buffer += chunk
-
-      // If current buffer value is valid JSON, clear buffer and run events
-      let body;
-      try {
-        body = JSON.parse(this.buffer)
-      } catch (e) { return }
-      this.buffer = ''
-      this.events.data.forEach((post) => post(body))
-    })
-  }
-
-  /* MetroConnection API */
-
-  on (event) {
-    return new MetroStream((post, kill) => {
-      if (Object.keys(this.events).indexOf(event) !== -1) {
-        this.events[event].push(post)
-        this.events.die.push(kill)
-      }
-    })
-  }
-
-  command (command) {
-    return this.on('data')
-               .filter((data) => data.is === command)
-               .replace((data) => {
-                 delete data.is
-                 return data
-               })
-  }
-}
+var MetroConnection = require('./MetroInternal/MetroConnection')
 
 var cxn = new MetroConnection();
 
-
 cxn.command('init')
-.do((data) => {
-  console.log('command: ', data)
-})
-cxn.on('data')
    .do((data) => {
-     console.log('on: ', data)
+     console.log('init: ', data)
    })
-
-//cxn.command('init', (data) => console.log(data))
